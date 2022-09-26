401e841dff2127552e8e63306df1267b77d54966
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-26 15:33:03 -0700

    Created State of Square so value changes to 'X' on click

--- src/index.js
+++ src/index.js
@@ -4,13 +4,35 @@ import './index.css';
 
 // Square Component - Renders a single <button>
 class Square extends React.Component {
+    /* In order for a Square to 'Remember' that it was clicked and to add an X to it, we utilize a state.
+     * React components can have a state by setting 'this.state' in their constructors.
+     *  
+     */
+    constructor(props) {  // We create a constructor to initialize the state
+        super(props);     // we call super(props) because Square is a sublcass of the Component class. 
+        this.state = {
+            value: null
+        };
+    }
+
     render() {
         return (
-            // Props --> propertys (parameter of a component)
-            // since the Square component looks like so: <Square value={i}>,
-            // we access the value via --> this.props.value!
-            <button className="square">
-                {this.props.value}
+            /* Props --> propertys (parameter of a component)
+             * since the Square component looks like so: <Square value={i}>,
+             * we access the value via --> this.props.value!
+             */
+            <button className="square"
+                /* Add an event listener to the button which SETS THE STATE (changes the value of the button) to X when clicked.
+                 * 
+                 * By calling 'this.setState' from an onClick handler in the Square component's render method...
+                 * we tell react to re-render that Square whenever its <button> has been clicked.
+                 * 
+                 * After the update...
+                 * The Square's this.state.value will be equal to 'X'!
+                 */
+                onClick={() => this.setState({ value: 'X' })}
+            >
+                {this.state.value}
             </button>
         );
     }
@@ -21,6 +43,8 @@ class Board extends React.Component {
     renderSquare(i) {
         // Here, we call the Square Component to generate buttons
         // We set the value of the button to equal i using the {} syntax.
+        // We are simply PASSING A PROP from a PARENT Board Component to the CHILD Square component.
+        // this is how information flows in React App! --> From parent to children. 
         return <Square value={i} />;
     }
 
@@ -61,8 +85,8 @@ class Game extends React.Component {
                 <div className="game-info">
                     <div>{/* status */}</div>
                     <ol>{/* TODO */}</ol>
-                </div>
-            </div>
+                </div >
+            </div >
         );
     }
 }
